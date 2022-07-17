7ebe11c8e89ac2f0315f0a4ca3bad3cb9bb0c5d0
Jarod Christiansen <jarodchristiansen2@gmail.com>
2022-07-17 15:31:16 -0700

    Added Animation wrapper to asset cards

--- components/assets/AssetCard.js
+++ components/assets/AssetCard.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { Image } from "react-bootstrap";
+import AssetCardAnimationWrapper from "./AssetCardAnimationWrapper";
 
 const AssetCard = ({ asset }) => {
   const [assetDetails, setAssetDetails] = useState();
@@ -10,29 +11,31 @@ const AssetCard = ({ asset }) => {
   const exploreLink = `/assets/${symbol}`;
 
   return (
-    <div
-      className={
-        "card text-center mb-3 border border-dark border-1 rounded shadow"
-      }
-    >
-      <div className={"card-body py-4"}>
-        <h5 className="card-title">{title || name || "Card Title"}</h5>
+    <AssetCardAnimationWrapper>
+      <div
+        className={
+          "card text-center mb-3 border border-dark border-1 rounded shadow"
+        }
+      >
+        <div className={"card-body py-4"}>
+          <h5 className="card-title">{title || name || "Card Title"}</h5>
 
-        <h6 className="card-subtitle mb-2 text-muted">
-          {symbol || "Card subtitle"}
-        </h6>
+          <h6 className="card-subtitle mb-2 text-muted">
+            {symbol || "Card subtitle"}
+          </h6>
 
-        <div>
-          <Image src={imageUrl || small} />
+          <div>
+            <Image src={imageUrl || small} />
+          </div>
+
+          <Link href={exploreLink}>
+            <button className={"bg-primary text-white rounded mt-4 px-4"}>
+              Explore
+            </button>
+          </Link>
         </div>
-
-        <Link href={exploreLink}>
-          <button className={"bg-primary text-white rounded mt-4 px-4"}>
-            Explore
-          </button>
-        </Link>
       </div>
-    </div>
+    </AssetCardAnimationWrapper>
   );
 };
 
