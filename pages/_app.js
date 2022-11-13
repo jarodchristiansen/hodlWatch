8c64ec3e80d9c908f11b79a668ec419fe759a407
Jarod Christiansen <jarodchristiansen2@gmail.com>
2022-11-13 16:17:36 -0700

    feat: walletconnect

--- pages/_app.js
+++ pages/_app.js
@@ -4,13 +4,24 @@ import { ApolloProvider } from "@apollo/client";
 import { Provider } from "next-auth/client";
 import client from "../apollo-client";
 import Layout from "../components/layout/layout";
+import { Web3Modal } from "@web3modal/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  const config = {
+    projectId: `${process.env.WALLET_CONNECT_ID}`,
+    theme: "dark",
+    accentColor: "default",
+    ethereum: {
+      appName: "web3Modal",
+    },
+  };
+
   return (
     <Provider session={pageProps.session} store={[]}>
       <ApolloProvider client={client}>
         <Layout>
           <Component {...pageProps} />
+          <Web3Modal config={config} />
         </Layout>
       </ApolloProvider>
     </Provider>
--- pages/user/[id].jsx
+++ pages/user/[id].jsx
@@ -1,22 +1,52 @@
 import styled from "styled-components";
 import { useSession } from "next-auth/client";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { MediaQueries } from "../../styles/MediaQueries";
+import {
+  Web3Button,
+  useConnectModal,
+  useAccount,
+  useBalance,
+  useProvider,
+  useSigner,
+} from "@web3modal/react";
+import PriceScreener from "../../components/commons/screener";
 
 const ProfilePage = () => {
   const [session, loading] = useSession();
+  const { account, isReady } = useAccount();
+
   const [user, setUser] = useState();
+  //   const [walletIsConnected, setWalletIsConnected] = useState(false);
+  const { isOpen, open, close } = useConnectModal();
 
   useEffect(() => {
-    if (session.user) {
+    if (session?.user) {
       setUser(session.user);
     }
   }, [loading]);
 
-  console.log({ user });
+  const walletIsConnected = useMemo(() => {
+    if (!account.status) return false;
+
+    return account.status !== "disconnected" && account.status !== "connecting"
+      ? true
+      : false;
+  }, [account]);
+
+  const {
+    data: tokenData,
+    error: fetchTokenError,
+    isLoading: fetchTokensLoading,
+    refetch: refetchTokoenData,
+  } = useBalance({ addressOrName: account?.address });
+
+  //   const { data, error, isLoading } = useSigner();
+  console.log({ tokenData });
 
   return (
     <CentralWrapper>
+      <PriceScreener />
       <UserDetailsCard>
         <div className="detail-header">
           <h2>User Details</h2>
@@ -31,12 +61,29 @@ const ProfilePage = () => {
           <h4>Email:</h4>
           <h4>{user?.email}</h4>
         </div>
-
-        {/* <div className="detail-row">
-          <h4>Some Text:</h4>
-          <h4>Stuff</h4>
-        </div> */}
       </UserDetailsCard>
+
+      {!walletIsConnected && (
+        <ConnectWalletCard>
+          <h4>It looks like your wallet isn't connected</h4>
+          <button className="standardized-button" onClick={open}>
+            Connect Your Wallet
+          </button>
+        </ConnectWalletCard>
+      )}
+
+      {!!walletIsConnected && (
+        <ConnectWalletCard onClick={open}>
+          <h6>{account?.address}</h6>
+          <div>
+            <h4>Balance:</h4>
+            <h4>
+              {tokenData?.formatted} {tokenData?.symbol}
+            </h4>
+          </div>
+          <Web3Button />
+        </ConnectWalletCard>
+      )}
     </CentralWrapper>
   );
 };
@@ -47,13 +94,29 @@ const CentralWrapper = styled.div`
   width: 100%;
   justify-content: center;
   align-items: center;
+  gap: 3rem;
+`;
+
+const ConnectWalletCard = styled.div`
+  width: 100%;
+  border: 2px solid black;
+  border-radius: 14px;
+  text-align: center;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  align-items: center;
+
+  @media ${MediaQueries.MD} {
+    width: 30rem;
+  }
 `;
 
 const UserDetailsCard = styled.div`
   width: 100%;
   border: 2px solid black;
   border-radius: 14px;
-  margin-top: 3rem;
 
   .detail-header {
     display: flex;
