71eb3a379cd78736546ead768419997d5ae2f997
Jarod Christiansen <jarodchristiansen2@gmail.com>
2022-11-16 11:57:38 -0700

    fix: fixing client

--- apollo-client.js
+++ apollo-client.js
@@ -8,7 +8,10 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const client = new ApolloClient({
   dataIdFromObject: (o) => o.id,
-  link: new HttpLink({ uri: "http://localhost:3000/api/graphql", fetch }),
+  link: new HttpLink({
+    uri: "https://hodl-watch.vercel.app/api/graphql",
+    fetch,
+  }),
   cache: new InMemoryCache(),
   //     {
   //   typePolicies: {
