f6963fd309b07b745d61d5d5c476394e616f9ca1
Jarod Christiansen <jarodchristiansen2@gmail.com>
2022-11-19 15:35:34 -0700

    fix header import

--- __tests__/assets/index.test.js
+++ __tests__/assets/index.test.js
@@ -1,7 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import Header from "../../components/layout/Header";
 import "@testing-library/jest-dom";
-import { useSession } from "next-auth/client";
 import AssetsPage from "../../pages/assets";
 import { MockedProvider } from "@apollo/client/testing";
 import GET_ASSETS from "../../helpers/queries/getAssets";
