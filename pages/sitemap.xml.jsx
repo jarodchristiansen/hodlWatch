73152134bba25a2deab77171b278a4d495a83c21
Jarod Christiansen <jarodchristiansen2@gmail.com>
2022-12-03 14:29:30 -0700

    fix: again

--- pages/sitemap.xml.jsx
+++ pages/sitemap.xml.jsx
@@ -35,19 +35,21 @@ export const getServerSideProps = async ({ res }) => {
 
   const allPaths = [...pagesPaths];
 
-  // TODO: Refactor to actually be dynamic
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
-  <script/>
-  
-  <url>
-  <loc>https://kuty.me/</loc>
-  <lastmod>2021-11-14</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.9</priority>
-  </url>
-  
-  </urlset>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${allPaths
+        .map((url) => {
+          return `
+            <url>
+              <loc>${url}</loc>
+              <lastmod>${new Date().toISOString()}</lastmod>
+              <changefreq>monthly</changefreq>
+              <priority>1.0</priority>
+            </url>
+          `;
+        })
+        .join("")}
+    </urlset>
   `;
 
   res.setHeader("Content-Type", "text/xml");
