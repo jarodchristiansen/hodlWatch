0d6949e1e78d23d28431f835d0f4331bc9ae9a26
Jarod Christiansen <jarodchristiansen2@gmail.com>
2022-12-03 14:35:55 -0700

    temp xml fix

--- pages/sitemap.xml.jsx
+++ pages/sitemap.xml.jsx
@@ -36,20 +36,26 @@ export const getServerSideProps = async ({ res }) => {
   const allPaths = [...pagesPaths];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${allPaths
-        .map((url) => {
-          return `
-            <url>
-              <loc>${url}</loc>
-              <lastmod>${new Date().toISOString()}</lastmod>
-              <changefreq>monthly</changefreq>
-              <priority>1.0</priority>
-            </url>
-          `;
-        })
-        .join("")}
-    </urlset>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+  <script/>
+
+  <url>
+  <loc>https://hodl-watch.vercel.app/</loc>
+  <lastmod>${new Date().toISOString()}</lastmod>
+  <changefreq>monthly</changefreq>
+  <priority>1.0</priority>
+  </url>
+  
+
+  <url>
+  <loc>https://hodl-watch.vercel.app/news</loc>
+  <lastmod>${new Date().toISOString()}</lastmod>
+  <changefreq>monthly</changefreq>
+  <priority>1.0</priority>
+  </url>
+
+  </urlset>
+   
   `;
 
   res.setHeader("Content-Type", "text/xml");
