4d03e5d7e9009b624301240490bb33c15bde4e89
Jarod Christiansen <jarodchristiansen2@gmail.com>
2022-12-18 14:50:42 -0700

    test second mapping

--- pages/sitemap.xml.jsx
+++ pages/sitemap.xml.jsx
@@ -33,8 +33,6 @@ export const getServerSideProps = async ({ res, context }) => {
     });
   }
 
-  console.log({ postPaths });
-
   // const staticPaths = fs
   //   .readdirSync("pages")
   //   .filter((staticPage) => {
@@ -61,45 +59,65 @@ export const getServerSideProps = async ({ res, context }) => {
 
   const dynamicPaths = [`${BASE_URL}/newsfeed`, `${BASE_URL}/user/[id]`];
 
-  const allPaths = [...pagesPaths];
+  const allPaths = [...pagesPaths, ...postPaths];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/1" xmlns:news="http://www.google.com/schemas/sitemap-news/1" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
   <script/>
-  <url>
-  <loc>https://hodl-watch.vercel.app/</loc>
-  <lastmod>${new Date().toISOString()}</lastmod>
-  <changefreq>monthly</changefreq>
-  <priority>1.0</priority>
-  </url>
-  
-  <url>
-  <loc>https://hodl-watch.vercel.app/news</loc>
-  <lastmod>${new Date().toISOString()}</lastmod>
-  <changefreq>monthly</changefreq>
-  <priority>1.0</priority>
-  </url>
-  <url>
-  <loc>https://hodl-watch.vercel.app/education</loc>
-  <lastmod>${new Date().toISOString()}</lastmod>
-  <changefreq>monthly</changefreq>
-  <priority>1.0</priority>
-  </url>
-  <url>
-  <loc>https://hodl-watch.vercel.app/education/fibonacci-retracement-indicator</loc>
-  <lastmod>${new Date().toISOString()}</lastmod>
-  <changefreq>monthly</changefreq>
-  <priority>1.0</priority>
-  </url>
-  <url>
-  <loc>https://hodl-watch.vercel.app/education/what-is-bitcoin</loc>
-  <lastmod>${new Date().toISOString()}</lastmod>
-  <changefreq>monthly</changefreq>
-  <priority>1.0</priority>
-  </url>
-  </urlset>
+  ${allPaths
+    .map((url) => {
+      return `
+        <url>
+          <loc>${url}</loc>
+          <lastmod>${new Date().toISOString()}</lastmod>
+          <changefreq>monthly</changefreq>
+          <priority>1.0</priority>
+        </url>
+      `;
+    })
+    .join("")}
+
+    </urlset>
    
-  `;
+    `;
+
+  //   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  //   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/1" xmlns:news="http://www.google.com/schemas/sitemap-news/1" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+  //   <script/>
+  //   <url>
+  //   <loc>https://hodl-watch.vercel.app/</loc>
+  //   <lastmod>${new Date().toISOString()}</lastmod>
+  //   <changefreq>monthly</changefreq>
+  //   <priority>1.0</priority>
+  //   </url>
+
+  //   <url>
+  //   <loc>https://hodl-watch.vercel.app/news</loc>
+  //   <lastmod>${new Date().toISOString()}</lastmod>
+  //   <changefreq>monthly</changefreq>
+  //   <priority>1.0</priority>
+  //   </url>
+  //   <url>
+  //   <loc>https://hodl-watch.vercel.app/education</loc>
+  //   <lastmod>${new Date().toISOString()}</lastmod>
+  //   <changefreq>monthly</changefreq>
+  //   <priority>1.0</priority>
+  //   </url>
+  //   <url>
+  //   <loc>https://hodl-watch.vercel.app/education/fibonacci-retracement-indicator</loc>
+  //   <lastmod>${new Date().toISOString()}</lastmod>
+  //   <changefreq>monthly</changefreq>
+  //   <priority>1.0</priority>
+  //   </url>
+  //   <url>
+  //   <loc>https://hodl-watch.vercel.app/education/what-is-bitcoin</loc>
+  //   <lastmod>${new Date().toISOString()}</lastmod>
+  //   <changefreq>monthly</changefreq>
+  //   <priority>1.0</priority>
+  //   </url>
+  //   </urlset>
+
+  //   `;
 
   res.setHeader("Content-Type", "text/xml");
   res.write(sitemap);
