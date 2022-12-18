ea209dbfb63f1e1bd8d058591e15c139e4286e38
Jarod Christiansen <jarodchristiansen2@gmail.com>
2022-12-18 14:57:23 -0700

    test

--- pages/sitemap.xml.jsx
+++ pages/sitemap.xml.jsx
@@ -49,45 +49,50 @@ export const getServerSideProps = async ({ res, context }) => {
   //     return `${BASE_URL}/${staticPagePath}`;
   //   });
 
-  //   const pagesDir = "pages/**/*.tsx";
-  //   let pagesPaths = await glob.sync(pagesDir);
+  const pagesDir = "pages/**/*.tsx";
+  let pagesPaths = await glob.sync(pagesDir);
 
-  //   pagesPaths = pagesPaths
-  //     .filter((path) => !path.includes("["))
-  //     .filter((path) => !path.includes("/_"))
-  //     .filter((path) => !path.includes("404"));
+  pagesPaths = pagesPaths
+    .filter((path) => !path.includes("["))
+    .filter((path) => !path.includes("/_"))
+    .filter((path) => !path.includes("404"));
 
-  //   const dynamicPaths = [`${BASE_URL}/newsfeed`, `${BASE_URL}/user/[id]`];
+  const dynamicPaths = [`${BASE_URL}/newsfeed`, `${BASE_URL}/user/[id]`];
 
-  //   const allPaths = [...pagesPaths, ...postPaths];
+  const allPaths = [...pagesPaths, ...postPaths];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/1" xmlns:news="http://www.google.com/schemas/sitemap-news/1" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
   <script/>
 
   <url>
-  <loc>https://hodl-watch.vercel.app</loc>
+  <loc>https://hodl-watch.vercel.app/</loc>
+  <lastmod>2022-12-18T21:49:11.415Z</lastmod>
   <changefreq>monthly</changefreq>
   <priority>1.0</priority>
   </url>
 
   <url>
   <loc>https://hodl-watch.vercel.app/news</loc>
+  <lastmod>2022-12-18T21:49:11.415Z</lastmod>
   <changefreq>monthly</changefreq>
   <priority>1.0</priority>
   </url>
 
   <url>
   <loc>https://hodl-watch.vercel.app/education</loc>
+  <lastmod>2022-12-18T21:49:11.415Z</lastmod>
   <changefreq>monthly</changefreq>
   <priority>1.0</priority>
   </url>
 
-  ${postPaths
+
+  ${allPaths
     .map((url) => {
       return `
         <url>
           <loc>${url}</loc>
+          <lastmod>${new Date().toISOString()}</lastmod>
           <changefreq>monthly</changefreq>
           <priority>1.0</priority>
         </url>
