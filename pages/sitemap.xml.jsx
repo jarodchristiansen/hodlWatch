5bb5f91084c807258971cb442ed8519e105b5508
Jarod Christiansen <jarodchristiansen2@gmail.com>
2022-12-18 08:40:58 -0700

    adds posts dynamically

--- pages/sitemap.xml.jsx
+++ pages/sitemap.xml.jsx
@@ -1,4 +1,5 @@
-import glob from "glob";
+import Post from "../db/models/post";
+import * as fs from "fs";
 
 const Sitemap = () => {
   return null;
@@ -7,76 +8,49 @@ const Sitemap = () => {
 export const getServerSideProps = async ({ res }) => {
   const BASE_URL = process.env.BASE_URL;
 
-  // const staticPaths = fs
-  //   .readdirSync("pages")
-  //   .filter((staticPage) => {
-  //     return ![
-  //       "api",
-  //       "product",
-  //       "_app.js",
-  //       "_document.js",
-  //       "404.js",
-  //       "sitemap.xml.js",
-  //     ].includes(staticPage);
-  //   })
-  //   .map((staticPagePath) => {
-  //     return `${BASE_URL}/${staticPagePath}`;
-  //   });
+  let query1 = await Post.find({});
 
-  const pagesDir = "pages/**/*.tsx";
-  let pagesPaths = await glob.sync(pagesDir);
+  let postPaths = query1.map((post) => {
+    return `${BASE_URL}/${post.section.toLowerCase()}${post.slug}`;
+  });
 
-  pagesPaths = pagesPaths
-    .filter((path) => !path.includes("["))
-    .filter((path) => !path.includes("/_"))
-    .filter((path) => !path.includes("404"));
+  const staticPaths = fs
+    .readdirSync("pages")
+    .filter((staticPage) => {
+      return ![
+        "sitemap.xml.jsx",
+        "404.js",
+        "auth.jsx",
+        "_app.js",
+        "_document.jsx",
+        "api",
+        "index.tsx",
+      ].includes(staticPage);
+    })
+    .map((staticPagePath) => {
+      return `${BASE_URL}/${staticPagePath.replace(".js", "")}`;
+    });
 
-  const dynamicPaths = [`${BASE_URL}/newsfeed`, `${BASE_URL}/user/[id]`];
+  const dynamicPaths = [...postPaths];
 
-  const allPaths = [...pagesPaths];
+  const allPaths = [...staticPaths, ...dynamicPaths];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/1" xmlns:news="http://www.google.com/schemas/sitemap-news/1" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
   <script/>
 
-  <url>
-  <loc>https://hodl-watch.vercel.app/</loc>
-  <lastmod>${new Date().toISOString()}</lastmod>
-  <changefreq>monthly</changefreq>
-  <priority>1.0</priority>
-  </url>
-  
-
-  <url>
-  <loc>https://hodl-watch.vercel.app/news</loc>
-  <lastmod>${new Date().toISOString()}</lastmod>
-  <changefreq>monthly</changefreq>
-  <priority>1.0</priority>
-  </url>
-
-
-  <url>
-  <loc>https://hodl-watch.vercel.app/education</loc>
-  <lastmod>${new Date().toISOString()}</lastmod>
-  <changefreq>monthly</changefreq>
-  <priority>1.0</priority>
-  </url>
-
-  <url>
-  <loc>https://hodl-watch.vercel.app/education/fibonacci-retracement-indicator</loc>
-  <lastmod>${new Date().toISOString()}</lastmod>
-  <changefreq>monthly</changefreq>
-  <priority>1.0</priority>
-  </url>
-
-
-  <url>
-  <loc>https://hodl-watch.vercel.app/education/what-is-bitcoin</loc>
-  <lastmod>${new Date().toISOString()}</lastmod>
-  <changefreq>monthly</changefreq>
-  <priority>1.0</priority>
-  </url>
-
+  ${allPaths
+    .map((url) => {
+      return `
+        <url>
+          <loc>${url}</loc>
+          <lastmod>${new Date().toISOString()}</lastmod>
+          <changefreq>monthly</changefreq>
+          <priority>1.0</priority>
+        </url>
+      `;
+    })
+    .join("")}
   </urlset>
    
   `;
