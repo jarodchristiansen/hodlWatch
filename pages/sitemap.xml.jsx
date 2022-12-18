cf6d1571a452b751c9d4177d09922f21bd524fa9
Jarod Christiansen <jarodchristiansen2@gmail.com>
2022-12-18 14:59:40 -0700

    minor cleanup

--- pages/sitemap.xml.jsx
+++ pages/sitemap.xml.jsx
@@ -33,22 +33,6 @@ export const getServerSideProps = async ({ res, context }) => {
     });
   }
 
-  // const staticPaths = fs
-  //   .readdirSync("pages")
-  //   .filter((staticPage) => {
-  //     return ![
-  //       "api",
-  //       "product",
-  //       "_app.js",
-  //       "_document.js",
-  //       "404.js",
-  //       "sitemap.xml.js",
-  //     ].includes(staticPage);
-  //   })
-  //   .map((staticPagePath) => {
-  //     return `${BASE_URL}/${staticPagePath}`;
-  //   });
-
   const pagesDir = "pages/**/*.tsx";
   let pagesPaths = await glob.sync(pagesDir);
 
@@ -103,44 +87,6 @@ export const getServerSideProps = async ({ res, context }) => {
     </urlset>
    
     `;
-
-  //   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  //   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/1" xmlns:news="http://www.google.com/schemas/sitemap-news/1" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
-  //   <script/>
-  //   <url>
-  //   <loc>https://hodl-watch.vercel.app/</loc>
-  //   <lastmod>${new Date().toISOString()}</lastmod>
-  //   <changefreq>monthly</changefreq>
-  //   <priority>1.0</priority>
-  //   </url>
-
-  //   <url>
-  //   <loc>https://hodl-watch.vercel.app/news</loc>
-  //   <lastmod>${new Date().toISOString()}</lastmod>
-  //   <changefreq>monthly</changefreq>
-  //   <priority>1.0</priority>
-  //   </url>
-  //   <url>
-  //   <loc>https://hodl-watch.vercel.app/education</loc>
-  //   <lastmod>${new Date().toISOString()}</lastmod>
-  //   <changefreq>monthly</changefreq>
-  //   <priority>1.0</priority>
-  //   </url>
-  //   <url>
-  //   <loc>https://hodl-watch.vercel.app/education/fibonacci-retracement-indicator</loc>
-  //   <lastmod>${new Date().toISOString()}</lastmod>
-  //   <changefreq>monthly</changefreq>
-  //   <priority>1.0</priority>
-  //   </url>
-  //   <url>
-  //   <loc>https://hodl-watch.vercel.app/education/what-is-bitcoin</loc>
-  //   <lastmod>${new Date().toISOString()}</lastmod>
-  //   <changefreq>monthly</changefreq>
-  //   <priority>1.0</priority>
-  //   </url>
-  //   </urlset>
-
-  //   `;
 
   res.setHeader("Content-Type", "text/xml");
   res.write(sitemap);
