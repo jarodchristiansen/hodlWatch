cd522f413456a8551cb4009fee432ce479b8611d
Jarod Christiansen <jarodchristiansen2@gmail.com>
2022-12-21 13:32:31 -0700

    localStorage mobile test

--- helpers/localStorage/index.ts
+++ helpers/localStorage/index.ts
@@ -1,43 +1,29 @@
 import cookieCutter from "cookie-cutter";
 const Cryptr = require("cryptr");
-import {isDesktop} from '../../helpers/device/ClientSide';
 
-export const StoreLocalKeys = (identity: string, values: string) => {
-  // Obfuscates values being saved
+export const StoreLocalKeys = (identity, values) => {
+  //TODO: add logic to obfuscate the values passed in/decode
   const cryptr = new Cryptr(process.env.NEXT_PUBLIC_SECRET);
 
   const encryptedMessage = cryptr.encrypt(values);
 
-  if (isDesktop()) {
-    cookieCutter.set(identity, encryptedMessage, {
-      path: "/",
-      maxAge: 2592000,
-      sameSite: true,
-    });
-  
-  } else {
-    localStorage.setItem(identity, encryptedMessage);
-  }
+  cookieCutter.set(identity, encryptedMessage, {
+    path: "/",
+    maxAge: 2592000,
+    sameSite: true,
+  });
+  // localStorage.setItem(identity, values);
 };
 
 export const GetLocalKeys = (identity) => {
+  //TODO: add logic to obfuscate the values passed in/decode
 
   const cryptr = new Cryptr(process.env.NEXT_PUBLIC_SECRET);
 
   let retrieved = cookieCutter.get(identity);
-  let decryptedMessage;
 
   if (retrieved) {
-    decryptedMessage = cryptr.decrypt(retrieved);
-
-    if (decryptedMessage) {
-      return decryptedMessage;
-    }
-
-  } else if (!retrieved) {
-    retrieved = localStorage.getItem(identity);
-
-    decryptedMessage = cryptr.decrypt(retrieved);
+    const decryptedMessage = cryptr.decrypt(retrieved);
 
     if (decryptedMessage) {
       return decryptedMessage;
@@ -45,4 +31,5 @@ export const GetLocalKeys = (identity) => {
   }
 
   return;
+  // return localStorage.getItem(identity);
 };
