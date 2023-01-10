c423ffe189297c75c64ae729360058798741d4c0
Jarod Christiansen <jarodchristiansen2@gmail.com>
2023-01-10 10:07:52 -0700

    header update

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -40,7 +40,7 @@ function Header() {
     { key: 1, route: "/assets", guarded: false, text: "Assets" },
     { key: 2, route: `/user/${id}`, guarded: false, text: "Profile" },
     { key: 3, route: "/news", guarded: false, text: "News" },
-    { key: 4, route: "/education", guarded: false, text: "Education" },
+    { key: 4, route: "/education", guarded: false, text: "Background" },
     !session && {
       key: 5,
       route: "/auth?path=SignIn",
