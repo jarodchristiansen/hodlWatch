e7f4e65d3228401d2a15ff9af280abb871ebb909
Jarod Christiansen <jarodchristiansen2@gmail.com>
2023-01-10 09:17:10 -0700

    chart styling

--- components/assets/Finance/Charts/Bitcoin/WAPCharts.tsx
+++ components/assets/Finance/Charts/Bitcoin/WAPCharts.tsx
@@ -10,6 +10,8 @@ import {
 // import FinanceChartModal from "./FinanceChartModal";
 import React from "react";
 import styled from "styled-components";
+import { FormatUnixTime } from "@/helpers/formatters/time";
+import { currencyFormat } from "@/helpers/formatters/currency";
 
 /**
  *
@@ -29,8 +31,14 @@ const WAPChart = ({ data }) => {
             <CartesianGrid strokeDasharray="3 3" />
             <YAxis dataKey="close" yAxisId="left-axis" />
             <YAxis dataKey="close" yAxisId="right-axis" orientation="right" />
-            <XAxis dataKey="time" />
-            <Tooltip />
+            <XAxis
+              dataKey="time"
+              tickFormatter={(val) => FormatUnixTime(val)}
+            />
+            <Tooltip
+              labelFormatter={(val) => FormatUnixTime(val)}
+              formatter={(value) => currencyFormat(value)}
+            />
             <defs>
               <linearGradient id="vwap" x1="0" y1="0" x2="0" y2="1">
                 <stop offset="70%" stopColor="#8884d8" stopOpacity={0.4} />
